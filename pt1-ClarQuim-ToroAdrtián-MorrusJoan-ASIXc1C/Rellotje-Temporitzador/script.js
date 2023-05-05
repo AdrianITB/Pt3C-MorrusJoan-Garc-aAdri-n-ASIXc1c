94f1eefda02c4908324aaaace1323ca551c27a16
JoanMorrus <123154730+JoanMorrus@users.noreply.github.com>
2023-05-05 22:14:54 +0000

    a

--- pt1-ClarQuim-ToroAdrtián-MorrusJoan-ASIXc1C/Rellotje-Temporitzador/script.js
+++ pt1-ClarQuim-ToroAdrtián-MorrusJoan-ASIXc1C/Rellotje-Temporitzador/script.js
@@ -1,23 +1,60 @@
-const digitalClock = document.getElementById("digital-clock");
-const timePicker = document.getElementById("time-picker");
-const countdownInput = document.getElementById("countdown-input");
-const startButton = document.getElementById("start-button");
-const stopButton = document.getElementById("stop-button");
-const soundEffect = document.getElementById("sound-effect");
-const audio = document.getElementById("audio");
-
-let countdown;
-let timer;
-let remainingTime;
-let endTime;
-
-function startCountdown() {
+function stopCountdown() {
   clearInterval(timer);
-  endTime = Date.now() + remainingTime * 1000;
-  updateClock();
-  timer = setInterval(updateClock, 1000);
-  startButton.disabled = true;
-  stopButton.disabled = false;
+  countdown = null;
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  audio.pause();
 }
 
-function
+function playSoundEffect() {
+  audio.src = soundEffect.value;
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function updateClock() {
+  remainingTime = Math.round((endTime - Date.now()) / 1000);
+  if (remainingTime < 0) {
+    clearInterval(timer);
+    countdown = null;
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    playSoundEffect();
+    return;
+  }
+  let hours = Math.floor(remainingTime / 3600);
+  let minutes = Math.floor((remainingTime % 3600) / 60);
+  let seconds = remainingTime % 60;
+  digitalClock.textContent = `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+}
+
+function padZero(number) {
+  return number.toString().padStart(2, "0");
+}
+
+startButton.addEventListener("click", () => {
+  if (countdown === null) {
+    if (timePicker.value) {
+      let [hours, minutes] = timePicker.value.split(":");
+      remainingTime = hours * 3600 + minutes * 60;
+    } else if (countdownInput.value) {
+      let [hours, minutes, seconds] = countdownInput.value.split(":");
+      remainingTime = hours * 3600 + minutes * 60 + seconds * 1;
+    }
+    if (remainingTime > 0) {
+      countdown = true;
+      startCountdown();
+    }
+  }
+});
+
+stopButton.addEventListener("click", () => {
+  if (countdown !== null) {
+    stopCountdown();
+  }
+});
+
+soundEffect.addEventListener("change", () => {
+  playSoundEffect();
+});
+
